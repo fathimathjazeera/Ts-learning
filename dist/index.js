66f1c31fda70c30f769771cd0faa05ac1d5914b6
fathimathjazeera <jezijazeera@gmail.com>
2024-01-09 21:54:03 +0530

    my typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let age = 10;
+function hello(data) {
+    return data;
+}
+const result = hello("it's hello function");
+console.log(result);
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+let age:number= 10
+
+function hello (data:string){
+    return data
+}
+
+
+
+const result=hello("it's hello function")
+
+console.log(result)
